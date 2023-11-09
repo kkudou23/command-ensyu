e445fe2c8fbb775334b2c4c845dba72bf2eabea0
kkudou <k.kudou.sys23@morijyobi.ac.jp>
2023-11-09 12:15:03 +0900

    一回の実行で二人分計算できるように変更

--- Main.java
+++ Main.java
@@ -3,29 +3,61 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("身長(cm)を入力してください。");
-		double height = Double.parseDouble(sc.nextLine());
-		System.out.println("体重(kg)を入力してください。");
-		double weight = Double.parseDouble(sc.nextLine());
-		double bmi = weight/((height/100)*(height/100));
-		double appropriate = (height/100)*(height/100)*22;
-		String message = null;
+		System.out.println("一人目の身長(cm)を入力してください。");
+		double heightA = Double.parseDouble(sc.nextLine());
+		System.out.println("一人目の体重(kg)を入力してください。");
+		double weightA = Double.parseDouble(sc.nextLine());
+		double bmiA = weightA/((heightA/100)*(heightA/100));
+		double appropriateA = (heightA/100)*(heightA/100)*22;
+		String messageA = null;
 		
-		System.out.println("BMIは" + String.format("%.2f", bmi) + "です。");
-		if (bmi < 18.5) {
-			message = "低体重(やせ)";
-		} else if (bmi < 25) {
-			message = "普通体重";
-		} else if (bmi < 30) {
-			message = "肥満(1度)";
-		} else if (bmi < 35) {
-			message = "肥満(2度)";
-		} else if (bmi < 40) {
-			message = "肥満(3度)";
+		System.out.println("----------");
+		
+		System.out.println("二人目の身長(cm)を入力してください。");
+		double heightB = Double.parseDouble(sc.nextLine());
+		System.out.println("二人目の体重(kg)を入力してください。");
+		double weightB = Double.parseDouble(sc.nextLine());
+		double bmiB = weightB/((heightB/100)*(heightB/100));
+		double appropriateB = (heightB/100)*(heightB/100)*22;
+		String messageB = null;
+		
+		System.out.println("----------");
+		
+		System.out.println("一人目のBMIは" + String.format("%.2f", bmiA) + "です。");
+		if (bmiA < 18.5) {
+			messageA = "低体重(やせ)";
+		} else if (bmiA < 25) {
+			messageA = "普通体重";
+		} else if (bmiA < 30) {
+			messageA = "肥満(1度)";
+		} else if (bmiA < 35) {
+			messageA = "肥満(2度)";
+		} else if (bmiA < 40) {
+			messageA = "肥満(3度)";
 		} else {
-			message = "肥満(4度)";
+			messageA = "肥満(4度)";
 		}
-		System.out.println("これは" + message + "にあたります。");
-		System.out.println("身長" + height + "cmの人の適正体重は約" + String.format("%.2f", appropriate) + "kgです。");
+		System.out.println("これは" + messageA + "にあたります。");
+		System.out.println("身長" + heightA + "cmの人の適正体重は約" + String.format("%.2f", appropriateA) + "kgです。");
+		
+		System.out.println("----------");
+		
+		System.out.println("二人目のBMIは" + String.format("%.2f", bmiB) + "です。");
+		if (bmiB < 18.5) {
+			messageB = "低体重(やせ)";
+		} else if (bmiB < 25) {
+			messageB = "普通体重";
+		} else if (bmiB < 30) {
+			messageB = "肥満(1度)";
+		} else if (bmiB < 35) {
+			messageB = "肥満(2度)";
+		} else if (bmiB < 40) {
+			messageB = "肥満(3度)";
+		} else {
+			messageB = "肥満(4度)";
+		}
+		System.out.println("これは" + messageB + "にあたります。");
+		System.out.println("身長" + heightB + "cmの人の適正体重は約" + String.format("%.2f", appropriateB) + "kgです。");
+	
 	}
 }
